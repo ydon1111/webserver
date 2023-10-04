c029dcbd75d820a3ed4fc14a39cbface7e6d880c
yeongdon <kyd5022@naver.com>
2023-10-04 18:14:59 +0900

    csv file 받아와야함

--- pybo/urls.py
+++ pybo/urls.py
@@ -20,4 +20,8 @@ urlpatterns = [
     path('answer/modify/<int:answer_id>/', answer_views.answer_modify, name='answer_modify'),
     path('answer/delete/<int:answer_id>/', answer_views.answer_delete, name='answer_delete'),
     path('answer/vote/<int:answer_id>/', answer_views.answer_vote, name='answer_vote'),
+
+
+    # demo 
+    
 ]

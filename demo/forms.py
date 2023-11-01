5d8bddb042e05ba76b74399326dd3ec20f7f30c0
yeongdon <kyd5022@naver.com>
2023-11-01 15:15:29 +0900

    demo ui 수정

--- demo/forms.py
+++ demo/forms.py
@@ -1,11 +1,7 @@
 from django import forms
 from demo.models import CSVData
 
-# class CSVUploadForm(forms.ModelForm):
-#     class Meta:
-#         model = CSVData
-#         fields = ["csv_info"]
 
 
 class UploadForm(forms.Form):
-    csv_file = forms.FileField()
+    ppi_file = forms.FileField()
--- demo/views/upload_views.py
+++ demo/views/upload_views.py
@@ -10,17 +10,13 @@ from ..forms import UploadForm
 from demo.models import CSVData
 
 
-
-
-
 def upload_csv(request):
     if request.method == 'POST':
+
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            csv_data = CSVData(csv_file=request.FILES['csv_file'])
+            csv_data = CSVData(csv_file=request.FILES['ppi_file'])
             csv_data.save()
-
-      
 
             return render(request, 'demo/success.html')
         
